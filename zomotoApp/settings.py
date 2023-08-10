2ae94c3303461e8160ba1646492b813903c0ce90
Phoenix043 <132455515+Phoenix043@users.noreply.github.com>
2023-08-10 10:07:48 +0530

    all done

--- zomotoApp/settings.py
+++ zomotoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o%5bj7$152!_-cnelpq&mazrp=ov@93gnrt7$!*0!6x3a*=+r2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
@@ -115,12 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Adjust the path as needed
+    os.path.join(BASE_DIR, 'static'),
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
